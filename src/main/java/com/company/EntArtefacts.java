95351bd329176d34cd41563ce488176a149a27fe
lywangj <98389800+lywangj@users.noreply.github.com>
2022-10-17 16:10:36 +0000

    Add files via upload

---
+++ src/main/java/com/company/EntArtefacts.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class EntArtefacts extends GameEntity {
+
+
+    public EntArtefacts(String name, String description) {
+        super(name, description);
+        type = Type.artefacts;
+    }
+}
